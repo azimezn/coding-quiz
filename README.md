6d82d9e0f0ead3fe2d6ffef0dc0464ea7a13cdf2
azimezn <117550865+azimezn@users.noreply.github.com>
2022-12-01 21:52:20 -0500

    added end screen for after the questions end

--- README.md
+++ README.md
@@ -14,12 +14,13 @@ Test your coding skills with this short quiz. You can also come back and see you
 ## Installation
 
 You can reach this webpage through any web browser. No installation is required.
+https://azimezn.github.io/coding-quiz/
 
 ## Usage
 
 Click the button "Start Quiz" and the questions will start to appear. There is a timer and the timer will go down 15 seconds every time you answer a question wrong. The remaining time in the end will be the score.
 
-![alt text](assets/images/screenshot.png)
+![screenshot of coding quiz main page](assets/images/screenshot.png)
 
 ## Credits
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,29 @@
-var startButton = document.querySelector(".start-button");
-var highScore = document.querySelector("#high-score");
-var timer = document.querySelector("#timer");
-var startScreen = document.querySelector(".start-screen");
-var questionArea = document.querySelector(".question-area")
-var highscoreList = document.querySelector("#highscore-list")
+// variables connected to the HTML
+var $startButton = document.querySelector(".start-button");
+var $highScore = document.querySelector("#high-score");
+var $timer = document.querySelector("#timer");
+var $startScreen = document.querySelector(".start-screen");
+var $questionArea = document.querySelector(".question-area")
+var $highscoreList = document.querySelector("#highscore-list")
+var $submitName = document.querySelector("submit-name")
 
+// elements created in javascript
 var h2El = document.createElement("h2")
 var ulEl = document.createElement("ul")
+var pEl = document.createElement("p")
 
-startButton.addEventListener('click', startQuiz);
-questionArea.addEventListener("click", checkAnswer);
-highScore.addEventListener("click", showHighScore);
+// event listeners
+$startButton.addEventListener('click', startQuiz);
+$questionArea.addEventListener("click", checkAnswer);
 
+// other global variables
 var secondsLeft = 100;
 var questionCount = 0;
 var score = 0;
 var initials = "";
 var timerInterval;
 
+// list of questions, their choices, and the correct answers
 var questionList = [
 
     {
@@ -51,17 +57,11 @@ var questionList = [
     },
 
 ]
-
-function showHighScore() {
-    localStorage.getItem(initials);
-    highscoreList.innerHTML = `<li>${initials + ": " + score}`;
-
-}
-
+// makes the timer go down
 function setTime() {
     timerInterval = setInterval(function () {
         secondsLeft--;
-        timer.textContent = "Time: " + secondsLeft;
+        $timer.textContent = "Time: " + secondsLeft;
 
         if (secondsLeft <= 0) {
             clearInterval(timerInterval);
@@ -69,8 +69,7 @@ function setTime() {
     }, 1000)
 }
 
-
-
+// checks if the clicked element is the correct answer or not
 function checkAnswer(event) {
     event.preventDefault();
     if (event.target.matches("li")) {
@@ -79,49 +78,58 @@ function checkAnswer(event) {
             console.log("answer is correct");
         } else {
             console.log("answer is wrong");
+            // subtracts 15 seconds for every wrong answer
             secondsLeft = secondsLeft - 15;
         }
+    // adds to the question counter so teh code knows to continue or stop
     questionCount++;
+    // next question
     presentQuestion();
     }
 }
 
 function presentQuestion() {
     
-
     console.log("question count " + questionCount)
+    // if quesions are not finished, continue
     if (questionCount < questionList.length) {
         h2El.textContent = questionList[questionCount].question;
         ulEl.innerHTML = `<li>${questionList[questionCount].choices[0]}</li><li>${questionList[questionCount].choices[1]}</li><li>${questionList[questionCount].choices[2]}</li><li>${questionList[questionCount].choices[3]}</li>`;
         //"<li>" + questionList[0].choices[0] + "</li><li>" + questionList[0].choices[1] + "</li><li>" + questionList[0].choices[2] + "</li><li>" + questionList[0].choices[3] + "</li>";
-        // checkAnswer();
+
     }
+    // if question count reaches max questions
     else {
         console.log("questions are finished")
-        score == secondsLeft;
-        timer == secondsLeft;
-        //clearInterval(timerInterval);
-        //final screen
-        questionArea.textContent = "";
-        questionArea.innerHTML = "<textarea></textarea>";
-        // enter initials in var initials
-        localStorage.setItem(initials, score);
-        console.log(initials, score);
+        console.log(score);
+
+        //clear the question for the results
+        $questionArea.textContent = "";
+  
+        // stop timer and the remaining time is the score
+        score = secondsLeft;
+        clearInterval(timerInterval)
+
+        // inserted element to display score
+        $questionArea.appendChild(pEl);
+        pEl.textContent = "score: " + score;
 
     }
 }
 
+// after clicking start quiz, the screen empties and ready for questions
 function startQuiz(event) {
     event.preventDefault();
     event.stopPropagation();
-    startScreen.textContent = "";
-    startScreen.innerHTML = "<h1>Coding Quiz</h1>";
-    questionArea.innerHTML = "";
+    $startScreen.textContent = "";
+    $startScreen.innerHTML = "<h1>Coding Quiz</h1>";
+    $questionArea.innerHTML = "";
     setTime();
 
-    questionArea.appendChild(h2El);
-    questionArea.appendChild(ulEl);
+    $questionArea.appendChild(h2El);
+    $questionArea.appendChild(ulEl);
 
+    // show first question
     presentQuestion();
 }
 
